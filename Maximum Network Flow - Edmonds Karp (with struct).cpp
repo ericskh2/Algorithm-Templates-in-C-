221eac2980dbf54b0d9b429fd0106c0a8ff90e08
kheslex2 <42265526+kheslex2@users.noreply.github.com>
2021-07-08 18:56:35 +0800

    Update Maximum Network Flow - Edmonds Karp (with struct).cpp

--- Maximum Network Flow - Edmonds Karp (with struct).cpp
+++ Maximum Network Flow - Edmonds Karp (with struct).cpp
@@ -1,5 +1,3 @@
-// source: https://oi-wiki.org/graph/flow/max-flow/
-
 #include <bits/stdc++.h>
 using namespace std;
 #define maxn 250
@@ -11,55 +9,75 @@ struct Edge {
     Edge(int u, int v, int c, int f) : from(u), to(v), cap(c), flow(f) {}
 };
 
-struct EK {
-    int n, m;
-    vector<Edge> edges;
-    vector<int> G[maxn];
-    int a[maxn], p[maxn];
 
-    void init(int n) {
-        for (int i = 0; i < n; i++) G[i].clear();
-        edges.clear();
-    }
+int n, m;
+vector<Edge> edges;
+vector<int> G[maxn];
+int a[maxn], p[maxn];
 
-    void AddEdge(int from, int to, int cap) {
-        edges.push_back(Edge(from, to, cap, 0));
-        edges.push_back(Edge(to, from, 0, 0));
-        m = edges.size();
-        G[from].push_back(m - 2);
-        G[to].push_back(m - 1);
-    }
+void init(int n) {
+    for (int i = 0; i < n; i++) G[i].clear();
+    edges.clear();
+}
 
-    int Maxflow(int s, int t) {
-        int flow = 0;
-        for (;;) {
-            memset(a, 0, sizeof(a));
-            queue<int> Q;
-            Q.push(s);
-            a[s] = INF;
-            while (!Q.empty()) {
-                int x = Q.front();
-                Q.pop();
-                for (int i = 0; i < G[x].size(); i++) {
-                    Edge& e = edges[G[x][i]];
-                    if (!a[e.to] && e.cap > e.flow) {
-                        p[e.to] = G[x][i]; 
-                        a[e.to] =
-                            min(a[x], e.cap - e.flow); 
-                        Q.push(e.to);
-                    }
+void addEdge(int from, int to, int cap) {
+    edges.push_back(Edge(from, to, cap, 0));
+    edges.push_back(Edge(to, from, 0, 0));
+    m = edges.size();
+    G[from].push_back(m - 2);
+    G[to].push_back(m - 1);
+}
+
+int maxFlow(int s, int t) {
+    int flow = 0;
+    for (;;) {
+        memset(a, 0, sizeof(a));
+        queue<int> Q;
+        Q.push(s);
+        a[s] = INF;
+        while (!Q.empty()) {
+            int x = Q.front();
+            Q.pop();
+            for (int i = 0; i < G[x].size(); i++) {
+                Edge& e = edges[G[x][i]];
+                if (!a[e.to] && e.cap > e.flow) {
+                    p[e.to] = G[x][i];
+                    a[e.to] =
+                        min(a[x], e.cap - e.flow);
+                    Q.push(e.to);
                 }
-                if (a[t]) break; 
             }
-            if (!a[t])
-                break; 
-            for (int u = t; u != s;
-                u = edges[p[u]].from) {  
-                edges[p[u]].flow += a[t];     
-                edges[p[u] ^ 1].flow -= a[t];  
-            }
-            flow += a[t];
+            if (a[t]) break;
         }
-        return flow;
+        if (!a[t])
+            break;
+        for (int u = t; u != s;
+            u = edges[p[u]].from) {
+            edges[p[u]].flow += a[t];
+            edges[p[u] ^ 1].flow -= a[t];
+        }
+        flow += a[t];
     }
-};
+    return flow;
+}
+
+int main() {
+
+    int n, m, s, t;
+    
+    while (cin>>n>>m>>s>>t) {
+
+        init(n + 1);
+
+        for (int i = 0; i < m; i++) {
+            ll u, v, w;
+            cin >> u >> v >> w;
+            addEdge(u, v, w);
+        }
+        
+        ll ans = maxFlow(s,t);
+
+        cout << ans << endl;
+    }
+    return 0;
+}
