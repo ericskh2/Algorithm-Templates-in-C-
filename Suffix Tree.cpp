0208f1a02e4948177b3be21e381694e2ba4582c4
kheslex2 <42265526+kheslex2@users.noreply.github.com>
2021-08-01 19:43:19 +0800

    Update Suffix Tree.cpp

--- Suffix Tree.cpp
+++ Suffix Tree.cpp
@@ -3,28 +3,34 @@ using namespace std;
 #define ll long long
 #define INF 0x3f3f3f3f
 
+const int maxn = 1000;
+
 struct suffix{
     int idx;
     int rnk;
     int nextrnk;
 };
 
+int sa[maxn+1];
+int ind[maxn+1];
+int height[maxn+1];
+int rnk[maxn+1];
+suffix suffixes[maxn+1];
+
 bool cmp(suffix a, suffix b){
-    if(a.rnk != b.rnk) return a.rnk < b.rnk;
+    if(a.rnk!=b.rnk) return a.rnk<b.rnk;
     return a.nextrnk < b.nextrnk;
 }
 
-void buildSuffixArray(vector<int>& sa, string& s){
+void buildSuffixArray(string& s){
     int n = s.size();
-    vector<suffix> suffixes(n);
     for(int i = 0;i<n;i++){
         suffixes[i].idx = i;
         suffixes[i].rnk = s[i];
-        suffixes[i].nextrnk = ((i + 1) < n) ? (s[i + 1]) : -1;
+        suffixes[i].nextrnk = ((i+1)<n) ? (s[i+1]) : -1;
     }
-    sort(suffixes.begin(),suffixes.end(),cmp);
+    sort(suffixes,suffixes+n,cmp);
 
-    vector<int> ind(n);
     for(int k = 4;k<2*n;k*=2){
         int rnk = 0;
         int prernk = suffixes[0].rnk;
@@ -32,7 +38,7 @@ void buildSuffixArray(vector<int>& sa, string& s){
         ind[suffixes[0].idx] = 0;
 
         for(int i = 1;i<n;i++){
-            if(suffixes[i].rnk == prernk && suffixes[i].nextrnk == suffixes[i - 1].nextrnk) {
+            if(suffixes[i].rnk == prernk && suffixes[i].nextrnk == suffixes[i-1].nextrnk){
                 prernk = suffixes[i].rnk;
                 suffixes[i].rnk = rnk;
             } else {
@@ -41,12 +47,11 @@ void buildSuffixArray(vector<int>& sa, string& s){
             }
             ind[suffixes[i].idx] = i;
         }
-
         for(int i = 0;i<n;i++){
             int nextidx = suffixes[i].idx + k/2;
-            suffixes[i].nextrnk = (nextidx < n) ? suffixes[ind[nextidx]].rnk : -1;
+            suffixes[i].nextrnk = (nextidx<n)?suffixes[ind[nextidx]].rnk:-1;
         }
-        sort(suffixes.begin(),suffixes.end(),cmp);
+        sort(suffixes,suffixes+n,cmp);
     }
 
     for(int i = 0;i<n;i++){
@@ -54,11 +59,9 @@ void buildSuffixArray(vector<int>& sa, string& s){
     }
 }
 
-void buildHeightArray(vector<int>& height, vector<int>& sa,string& s){
+void buildHeightArray(string& s){
     int n = s.size();
     int k = 0;
-
-    vector<int> rnk(n,0);
 
     for(int i = 0;i<n;i++) rnk[sa[i]] = i;
 
@@ -73,8 +76,7 @@ void buildHeightArray(vector<int>& height, vector<int>& sa,string& s){
         while(i+k<n && j+k<n && s[i+k] == s[j+k]) k++;
 
         height[rnk[i]] = k;
-
-        if(k) k--;
+        if(k)k--;
     }
 
 }
@@ -83,43 +85,46 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-
     int t;
     cin>>t;
-    while(t--){
+    for(int tc = 1;tc<=t;tc++){
         string in;
         cin>>in;
 
         int n = in.size();
-        vector<int> sa(n);
-        vector<int> height(n);
-        buildSuffixArray(sa,in);
-        buildHeightArray(height,sa,in);
+        buildSuffixArray(in);
+        buildHeightArray(in);
 
         string ans = "";
-        int times = 0;
-        int temp = 1;
-        for(int i = 1;i<n;i++){
-            if(height[i]){
-                if(height[i]==height[i-1]){
-                    temp++;
-                    if(height[i]>ans.size() || (height[i]==ans.size() && in.substr(sa[i],height[i])<=ans)){
-                        ans = in.substr(sa[i],height[i]);
-                        times = temp+1;
-                    }
-                } else {
-                    temp = 1;
-
-                    if(height[i]>ans.size() || (height[i]==ans.size() && in.substr(sa[i],height[i])<=ans)){
-                        ans = in.substr(sa[i],height[i]);
-                        times = temp+1;
-                    }
+        int mx = 0;
+        int mxcnt = 0;
+        for(int i = 0;i<n;i++){
+            if(height[i]>mx){
+                mx = height[i];
+                int cnt = 2;
+                while(i<n-1 && height[i]==height[i+1]){
+                    i++;
+                    cnt++;
+                }
+                mxcnt = cnt;
+                ans = in.substr(sa[i],mx);
+            } else if(height[i]==mx){
+                int cnt = 2;
+                while(i<n-1 && height[i]==height[i+1]){
+                    i++;
+                    cnt++;
+                }
+                if(in.substr(sa[i],mx)<ans){
+                    ans = in.substr(sa[i],mx);
+                    mxcnt = cnt;
                 }
             }
-
         }
-        (!ans.empty()) ? cout<<ans<<" "<<times<<endl : cout<<"No repetitions found!"<<endl;
+        if(mx>0){
+            cout<<ans<<" "<<mxcnt<<endl;
+        } else {
+            cout<<"No repetitions found!"<<endl;
+        }
     }
-
     return 0;
 }
